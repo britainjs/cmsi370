edd3cd9d75cca9a7706215ae5ff341f07d46aa0a
Britainjs <bsouthwi@lion.lmu.edu>
2012-11-28 01:21:42 -0800

    Forgot to save the cleanup on the code.

--- direct-manipulation/boxes/boxes.js
+++ direct-manipulation/boxes/boxes.js
@@ -45,6 +45,7 @@ var Boxes = {
                 .appendTo(this)
                 .addClass("box")
                 .offset({ left: this.anchorX, top: this.anchorY });
+            // Add a handle to the box to indicate where to grab for resizing.
             this.drawingBox.handle = $("<div></div>")
                 .appendTo(this.drawingBox)
                 .addClass("handle");
@@ -59,7 +60,6 @@ var Boxes = {
      * Tracks a box as it is rubberbanded or moved across the drawing area.
      */
     trackDrag: function (event) {
-        //console.log(this.resizingBox);
         // Don't bother if we aren't tracking anything.
         // JD: Have you tried console.log(this.resizingBox) to see if there
         //     really is a this.resizingBox after initiating a resize?
@@ -76,18 +76,16 @@ var Boxes = {
                 .width(Math.abs(event.pageX - this.anchorX))
                 .height(Math.abs(event.pageY - this.anchorY));
         }else if (this.resizingBox) {
-           //console.log(this);
+           // Behaves just like drawing with different anchors.
            var newOffset = {
                 left: (this.anchorX < event.pageX) ? this.anchorX : event.pageX,
                 top: (this.anchorY < event.pageY) ? this.anchorY : event.pageY
             };
-            
+            // Set the dimensions of the box as defined by the cursor
             this.resizingBox
                 .offset(newOffset)
                 .width(Math.abs(event.pageX - this.anchorX))
-                .height(Math.abs(event.pageY - this.anchorY));        
-                
-                
+                .height(Math.abs(event.pageY - this.anchorY));              
         } else if (this.movingBox) {
             // Reposition the object.
             if( (event.pageX > $(".drawing-area").width()) | 
@@ -128,6 +126,7 @@ var Boxes = {
 
             // JD: 512 is an avoidable hardcode!  What if the web designer
             //     changes the drawing area CSS behind your back?
+            // Delete the box if it is outside the drawing area.
             if( (event.pageX > $(".drawing-area").width()) | 
                 (event.pageY > $(".drawing-area").height())){
                     this.movingBox.remove();
@@ -198,19 +197,22 @@ var Boxes = {
     // Only resize on left mouse button.
     
         if(event.which === Boxes.LEFT_BUTTON ) {
+        
+            // Grab the handle.
             var jThis = $(this);
-
             // JD: Take note---what object "owns" the resizingBox that is
-            //     being assigned below?   
+            //     being assigned below?
+            // Grab the box   
             var parentBox = jThis.parent().get(0);
+            // Grab the drawing area
             var superParent = $(parentBox).parent().get(0);
+            // Set the drawing area's state to show it is resizing.
             superParent.resizingBox = $(parentBox);
             // JD: Also, no need to $() something that was already $()'ed.
-            superParent.anchorX = event.pageX
-                - $(parentBox).width();
-            superParent.anchorY = event.pageY
-                - $(parentBox).height();
-                
+            // Set the anchors to the upper right corner.
+            superParent.anchorX = event.pageX - $(parentBox).width();
+            superParent.anchorY = event.pageY - $(parentBox).height();
+    
             Boxes.setupDragState();
             
             event.stopPropagation();
