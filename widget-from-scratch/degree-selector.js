63dcb98f7a22262f588f54931824d8e7b2482291
Britainjs <bsouthwi@lion.lmu.edu>
2012-11-30 01:42:38 -0800

    Changed the css a bit. Added code to append part of a drop down menu to the widget.

--- widget-from-scratch/degree-selector.js
+++ widget-from-scratch/degree-selector.js
@@ -4,6 +4,15 @@
             var selector = $("<div>Degree</div>")
                 .addClass("selector")
                 .appendTo($(this));
+            var menu = $("<select></select>")
+                .addClass("menu")
+                .appendTo(selector);
+            var bs = $("<option>BS</option>")
+                .addClass("menuItem")
+                .appendTo(menu);
+            var ba = $("<option>BA</option>")
+                .addClass("menuItem")
+                .appendTo(menu);
         });
     
     };
