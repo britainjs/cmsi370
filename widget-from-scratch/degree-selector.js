280c51fb39573b2b4a86acb2bfb12a98f758461f
Britainjs <bsouthwi@lion.lmu.edu>
2012-12-01 10:50:53 -0800

    More css edits. The affordance arrow can now close the menu and reset it to the previous selection.

--- widget-from-scratch/degree-selector.js
+++ widget-from-scratch/degree-selector.js
@@ -6,7 +6,7 @@
                 .addClass("selector")
                 .appendTo($(this));
             var bs = $("<span>BS</span>")
-                .addClass("menu-item")
+                .addClass("menu-item previous")
                 .appendTo(selector)
                 .css("display", "inline-block");
             var ba = $("<span>BA</span>")
@@ -40,25 +40,40 @@ $(".arrow").mousedown(dropDown);
 function dropDown(){
     /*Grab the parent of the selected element so we can get all of the elements in the
       list*/
+    
+    $(this).addClass("previous");
     var parent = $(this).parent().get(0);
     var currentMenu = $($(parent).children().get());
+    currentMenu.unbind("mousedown");
     //Make the elements visible.
     currentMenu.css("display", "inline-block");
     //Push them to the front.
     $(parent).css("z-index", "1000");
     //Get rid of the dropDown listener and put in a selection listener.
-    currentMenu.unbind("mousedown");
+    
     currentMenu.mousedown(select);
     $(".arrow").unbind("mousedown");
+    $(".arrow").mousedown(function(){
+        $(".selector").css("z-index", "0");
+        currentMenu.css("display", "none");
+        $(".previous").css("display", "inline-block");
+        currentMenu.unbind("mousedown");
+        $(".menu-item").mousedown(dropDown); 
+        $(".arrow").css("display", "block");
+        $(".arrow").mousedown(dropDown);
+    });
+        
 }
 
 function select(){
     var parent = $(this).parent().get(0);
     var currentMenu = $($(parent).children().get());
+    $("span").removeClass("previous");
     $(".selector").css("z-index", "0");
     //Hide all of the elements except the selected element.
     currentMenu.css("display", "none");
     $(this).css("display", "inline-block");
+    $(this).addClass("previous");
     //Restore the dropDown listener and push the menu back.
     currentMenu.unbind("mousedown");
     $(".menu-item").mousedown(dropDown); 
